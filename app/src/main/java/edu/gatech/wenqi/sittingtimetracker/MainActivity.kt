2ca6b634b3f80f819fbe2a1203afc9948c411246
Wenqi He <hewenqi96@gmail.com>
2019-01-30 01:11:10 -0500

    Move sensor code to foreground service

--- app/src/main/java/edu/gatech/wenqi/sittingtimetracker/MainActivity.kt
+++ app/src/main/java/edu/gatech/wenqi/sittingtimetracker/MainActivity.kt
@@ -1,52 +1,16 @@
 package edu.gatech.wenqi.sittingtimetracker
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.absoluteValue
 
-class MainActivity: AppCompatActivity(), SensorEventListener {
-    private var vertUnitVec: FloatArray = FloatArray(3)
-    private var acceleration: FloatArray = FloatArray(3)
-    private var prevAcc: Float = 0.0.toFloat()
+class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val accelerometer: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
-        val gravitySensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
-        // Register self as event listener
-        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST)
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
-    }
-
-    // Implement SensorEventListener interface
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
-    override fun onSensorChanged(event: SensorEvent?) {
-        event?.let {
-            val sensor:Sensor = it.sensor
-            when(sensor.type) {
-                Sensor.TYPE_LINEAR_ACCELERATION -> this.acceleration = it.values
-                Sensor.TYPE_GRAVITY -> this.vertUnitVec = it.values.map { v -> (v / 9.81).toFloat() }.toFloatArray()
-            }
-        }
-
-        val curAcc: Float = vertUnitVec[0] * acceleration[0] + vertUnitVec[1] * acceleration[1] + vertUnitVec[2] * acceleration[2]
-        Log.i("acceleration", "$curAcc")
-        if (curAcc.absoluteValue > 3.0) { // Threshold??
-            if (prevAcc >= 0 && curAcc < 0) {
-                textView.text = "위"
-            } else if (prevAcc <= 0 && curAcc > 0) {
-                textView.text = "아래"
-            }
-            prevAcc = curAcc
+        Intent(this, SensorService::class.java).also {
+            startService(it)
         }
     }
 
---
+++ app/src/main/java/edu/gatech/wenqi/sittingtimetracker/SensorService.kt
@@ -0,0 +1,78 @@
+package edu.gatech.wenqi.sittingtimetracker
+
+import android.annotation.TargetApi
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.IBinder
+import android.util.Log
+import kotlin.math.absoluteValue
+
+@TargetApi(26)
+class SensorService : Service(), SensorEventListener {
+    private var notificationManager: NotificationManager? = null
+    private var vertUnitVec: FloatArray = FloatArray(3)
+    private var acceleration: FloatArray = FloatArray(3)
+    private var prevAcc: Float = 0.0.toFloat()
+
+    override fun onCreate() {
+        super.onCreate()
+        this.notificationManager = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        val gravitySensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        // Register self as event listener
+        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_LOW).also {
+            this.notificationManager?.createNotificationChannel(it)
+        }
+        val intent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+        val notification = Notification.Builder(this, "default")
+            .setContentTitle("Tracking Sitting Time")
+            .setContentText("This might drain your battery")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(intent)
+            .build()
+        this.startForeground(1, notification)
+        return Service.START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    // Implement SensorEventListener interface
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            val sensor:Sensor = it.sensor
+            when(sensor.type) {
+                Sensor.TYPE_LINEAR_ACCELERATION -> this.acceleration = it.values
+                Sensor.TYPE_GRAVITY -> this.vertUnitVec = it.values.map { v -> (v / 9.81).toFloat() }.toFloatArray()
+            }
+        }
+
+        val curAcc: Float = vertUnitVec[0] * acceleration[0] + vertUnitVec[1] * acceleration[1] + vertUnitVec[2] * acceleration[2]
+        if (curAcc.absoluteValue > 3.0) { // Threshold??
+            Log.i("acceleration", "$curAcc")
+            // TODO: SEND MESSAGE TO ACTIVITY
+            if (prevAcc >= 0 && curAcc < 0) {
+//                textView.text = "위"
+            } else if (prevAcc <= 0 && curAcc > 0) {
+//                textView.text = "아래"
+            }
+            prevAcc = curAcc
+        }
+    }
+}
